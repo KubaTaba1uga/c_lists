cea82c0c0b14e073043dc69ff9a0babd0d833cd4
KubaTaba1uga <jakub@buczynski.net.pl>
2023-09-23 09:18:38 +0200

    save progress, before simplifying setUp

--- test/test_utils.d/test_pointers_utils.c
+++ test/test_utils.d/test_pointers_utils.c
@@ -1,17 +1,26 @@
+/*******************************************************************************
+ *    IMPORTS
+ ******************************************************************************/
+// C standard library
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "pointers_utils.h"
+// Test framework
 #include "unity.h"
 
-char **values,
-    **values_ptr_cp; // = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena",
-                     // "", "", "", ""};
-size_t values_len;
+// App
+#include "pointers_utils.h"
 
-void **memory_mock_values;
+/*******************************************************************************
+ *    TESTS DATA
+ ******************************************************************************/
+char *values_content[] = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", "",
+                          "",           ""};
+size_t values_len = sizeof(values_content) / sizeof(char *);
+char **values = NULL, **values_cp = NULL;
+void **values_ptr_cp = NULL;
 
 char **alloc_values(size_t n) {
   size_t values_size = n * sizeof(char *);
@@ -45,7 +54,8 @@ char **alloc_values_elements(size_t n, char *content[n], char **values) {
 
 void free_values(size_t n, char *values[n]) {
   for (size_t i = 0; i < n; i++)
-    free(values[i]);
+    if (values[i])
+      free(values[i]);
 
   free(values);
 }
@@ -53,31 +63,24 @@ void free_values(size_t n, char *values[n]) {
 // TO-DO refacotr setUp and tearDown, setUp make mallocs tearDown make frees
 //            configure their own environment using memory mocks.
 void setUp() {
-  char *values_content[] = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", "",
-                            "",           ""};
-  size_t i;
-  values_len = sizeof(values_content) / sizeof(char *);
+  char **mem, ***arrays_to_fill[] = {&values, &values_cp};
 
-  values = alloc_values(values_len);
-  values = alloc_values_elements(values_len, values_content, values);
-  for (i = 0; i < values_len; i++)
-    strcpy(values[i], values_content[i]);
+  for (size_t i = 0; i < sizeof(arrays_to_fill) / sizeof(char *); i++) {
+    mem = alloc_values(values_len);
+    mem = alloc_values_elements(values_len, values_content, mem);
+    for (size_t i = 0; i < values_len; i++)
+      strcpy(mem[i], values_content[i]);
 
-  // Copy values pointers to values_cp
-  values_ptr_cp = alloc_values(values_len);
-
-  for (size_t i = 0; i < values_len; i++) {
-    values_ptr_cp[i] = values[i];
+    *(arrays_to_fill[i]) = mem;
   }
 
-  // Allocate mock
-  memory_mock_values = (void **)alloc_values(values_len);
+  values_ptr_cp = calloc(values_len, values_len * sizeof(char *));
 }
-void tearDown() {
 
-  /* free_values(values_len, values); */
-  /* free_values(values_len, values_ptr_cp); */
-  /* free(memory_mock_values); */
+void tearDown() {
+  free_values(values_len, values);
+  free_values(values_len, values_cp);
+  free_values(values_len, values_ptr_cp);
 }
 
 void print_values(size_t n, char *values[n]) {
@@ -90,14 +93,15 @@ void print_values(size_t n, char *values[n]) {
 }
 
 void test_move_pointers_array_success(void) {
-  char **dest = memory_mock_values, **src = values;
+  char **dest = (char **)values_ptr_cp, **src = values;
 
   move_pointers_array((void **)dest, (void **)src, values_len);
 
-  TEST_ASSERT_EQUAL_PTR_ARRAY(values_ptr_cp, dest, values_len);
-
   for (size_t i = 0; i < values_len; i++)
     TEST_ASSERT_NULL(src[i]);
+
+  for (size_t i = 0; i < values_len; i++)
+    TEST_ASSERT_EQUAL_STRING(values_content[i], dest[i]);
 }
 
 void test_move_pointers_array_overlapping_src_one_element_before_dest(void) {
@@ -116,47 +120,51 @@ void test_move_pointers_array_overlapping_src_one_element_before_dest(void) {
     TEST_ASSERT_EQUAL_STRING(expected[i], src[i]);
 }
 
-/* This scenario is hard to follow and pretty misleading, function name
- *  is not `nullify_array` afterall. That's why it's described as undefined.
- * Key to understand why all values are NULL, is in first two iterations.
- * 1. values = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", "", NULL,}
- * 2. values = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", NULL, NULL,}
+/* /\* This scenario is hard to follow and pretty misleading, function name */
+/*  *  is not `nullify_array` afterall. That's why it's described as undefined.
  */
-void test_move_pointers_array_overlapping_dest_one_element_before_src(void) {
-  char *expected[] = {"", NULL, NULL, NULL, NULL, NULL, NULL};
+/*  * Key to understand why all values are NULL, is in first two iterations. */
+/*  * 1. values = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", "", NULL,} */
+/*  * 2. values = {"MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", NULL, NULL,}
+ */
+/*  *\/ */
+/* void test_move_pointers_array_overlapping_dest_one_element_before_src(void) {
+ */
+/*   char *expected[] = {"", NULL, NULL, NULL, NULL, NULL, NULL}; */
 
-  char **src, **dest = values;
-  src = dest + 1;
+/*   char **src, **dest = values; */
+/*   src = dest + 1; */
 
-  move_pointers_array((void **)dest, (void **)src, values_len - 1);
+/*   move_pointers_array((void **)dest, (void **)src, values_len - 1); */
 
-  TEST_ASSERT_EQUAL_STRING(expected[0], values[0]);
+/*   TEST_ASSERT_EQUAL_STRING(expected[0], values[0]); */
 
-  for (size_t i = 1; i < values_len; i++)
-    TEST_ASSERT_NULL(values[i]);
-}
+/*   for (size_t i = 1; i < values_len; i++) */
+/*     TEST_ASSERT_NULL(values[i]); */
+/* } */
 
-void test_move_pointers_array_overlapping_src_multi_element_before_dest(void) {
-  char *expected[] = {
-      NULL, NULL, NULL, "MumboJumbo", "Kukuryku", "EeeeeeMakarena", "",
-  };
+/* void test_move_pointers_array_overlapping_src_multi_element_before_dest(void)
+ * { */
+/*   char *expected[] = { */
+/*       NULL, NULL, NULL, "MumboJumbo", "Kukuryku", "EeeeeeMakarena", "", */
+/*   }; */
 
-  char **dest, **src = values;
-  dest = src + 3;
+/*   char **dest, **src = values; */
+/*   dest = src + 3; */
 
-  move_pointers_array((void **)dest, (void **)src, values_len - 3);
+/*   move_pointers_array((void **)dest, (void **)src, values_len - 3); */
 
-  print_values(values_len, values);
+/*   print_values(values_len, values); */
 
-  TEST_ASSERT_NULL(src[0]);
+/*   TEST_ASSERT_NULL(src[0]); */
 
-  for (size_t i = 0; i < values_len; i++) {
-    if (!expected[i])
-      TEST_ASSERT_NULL(expected[i]);
-    else
-      TEST_ASSERT_EQUAL_STRING(expected[i], values[i]);
-  }
-}
+/*   for (size_t i = 0; i < values_len; i++) { */
+/*     if (!expected[i]) */
+/*       TEST_ASSERT_NULL(expected[i]); */
+/*     else */
+/*       TEST_ASSERT_EQUAL_STRING(expected[i], values[i]); */
+/*   } */
+/* } */
 
 /* void test_move_pointers_array_overlapping_src_one_element_before_dest(void)
  * {} */
