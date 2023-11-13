8842665ba847f39fe9ea18823cb9f0b2c6320f16
KubaTaba1uga <jakub@buczynski.net.pl>
2023-11-13 16:44:18 +0100

    fix bug

--- test/test_ar_list.d/test_ar_list_logic.c
+++ test/test_ar_list.d/test_ar_list_logic.c
@@ -60,17 +60,13 @@ void setUp(void) {
 
 void tearDown(void) {
 
-  if (array_memory_mock)
-    free(array_memory_mock);
+  free(array_memory_mock);
 
-  if (list_memory_mock)
-    free(list_memory_mock);
+  free(list_memory_mock);
 
   array_memory_mock = NULL;
-  array_memory_mock = 0;
 
   list_memory_mock = NULL;
-  list_memory_mock = 0;
 }
 
 /*******************************************************************************
@@ -504,8 +500,6 @@ void test_arl_create_memory_failure_list(void) {
   app_malloc_ExpectAndReturn(array_memory_mock_size, array_memory_mock);
   app_malloc_ExpectAndReturn(list_memory_mock_size, NULL);
   app_free_Expect(array_memory_mock);
-
-  array_memory_mock = NULL;
 
   err = arl_create(&l, 10);
 
