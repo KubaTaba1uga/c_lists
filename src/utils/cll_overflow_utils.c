a43a37a82842f3285affd6052d1ce7e71b78d69e
KubaTaba1uga <jakub@buczynski.net.pl>
2023-11-17 09:00:17 +0100

    fix bugs

--- src/utils/cll_overflow_utils.c
+++ src/utils/cll_overflow_utils.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#include "../common/cll_def.h"
+#include "c_lists/cll_def.h"
 
 /*******************************************************************************
  *    PRIVATE API
