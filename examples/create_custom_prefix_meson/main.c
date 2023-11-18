7b738ea392c36410f1accd833c725f840bbdb011
KubaTaba1uga <jakub@buczynski.net.pl>
2023-11-18 18:25:50 +0100

    fix bug in example

--- examples/create_custom_prefix_meson/main.c
+++ examples/create_custom_prefix_meson/main.c
@@ -1,4 +1,4 @@
-/* #include "chr_list.h" */
+#include "chr_list.h"
 #include "int_list.h"
 
 #include <stddef.h>
@@ -9,31 +9,39 @@ int main(void) {
   char chr_data[] = "abcbcbcbcb";
   int int_data[] = {1, 2, 3, 4, 5};
 
-  /* chr_ptr cl; */
-  /* int_ptr il; */
+  chr_ptr cl;
+  int_ptr il;
 
-  /* size_t i; */
-  /* int err; */
+  size_t i;
+  int err;
 
-  /* err = chr_create(&cl, 100); */
-  /* if (err) { */
-  /*   puts(chr_strerror(err)); */
-  /*   return -1; */
-  /* } */
+  err = chr_create(&cl, 100);
+  if (err) {
+    puts(chr_strerror(err));
+    return -1;
+  }
 
-  /* err = int_create(&il, 100); */
-  /* if (err) { */
-  /*   puts(int_strerror(err)); */
-  /*   return -2; */
-  /* } */
+  err = int_create(&il, 100);
+  if (err) {
+    puts(int_strerror(err));
+    return -2;
+  }
 
-  /* for (i = 0; i < strlen(chr_data); i++) { */
-  /*   err = cal_append(l, &string[i]); */
-  /*   if (err) { */
-  /*     puts(cll_strerror(err)); */
-  /*     return -2; */
-  /*   } */
-  /* } */
+  for (i = 0; i < strlen(chr_data); i++) {
+    err = chr_append(cl, chr_data[i]);
+    if (err) {
+      puts(chr_strerror(err));
+      return -3;
+    }
+  }
+
+  for (i = 0; i < sizeof(int_data) / sizeof(int); i++) {
+    err = int_append(il, int_data[i]);
+    if (err) {
+      puts(chr_strerror(err));
+      return -4;
+    }
+  }
 
   printf("Success\n");
 
