29be236ec00c10ce98901dbd2b13b087b368c3c7
KubaTaba1uga <jakub@buczynski.net.pl>
2023-11-20 08:42:31 +0100

    regenerate type

--- scripts/generate_sources.py
+++ scripts/generate_sources.py
@@ -3,12 +3,21 @@ import os
 import sys
 from pathlib import Path
 
-if len(sys.argv) < 3:
-    raise ValueError("Not enough arguments!")
+if len(sys.argv) < 4:
+    raise ValueError(
+        """Not enough arguments!
+Syntax: python3 generate_sources.py <source file> <new prefix> <new type> (<dest dir>)
+"""
+    )
 
 
+DEFAULT_TYPE = "ARL_VALUE_TYPE"
 DEFAULT_PREFIX = "arl_"
-NEW_PREFIX = sys.argv[2] + "_"  # os.environ["NEW_PREFIX"]
+
+file_path = sys.argv[1]
+new_prefix = sys.argv[2] + "_"
+new_type = sys.argv[3]
+
 
 _THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 _SRC_DIR = os.path.join(_THIS_DIR, "..", "src")
@@ -17,30 +26,35 @@ _HEADERS_DIR = os.path.join(_THIS_DIR, "..", "include")
 
 def regenerate_file(file_path):
     with open(file_path, "r") as src_fp:
-        source = src_fp.read()
+        old_content = src_fp.read()
 
-    if len(sys.argv) == 4:
-        file_path = os.path.join(sys.argv[3], os.path.basename(file_path))
+    new_content = regenerate_content(old_content)
 
-    # New file's source creation
-    new_content = source.replace(DEFAULT_PREFIX.lower(), NEW_PREFIX.lower())
-    new_content = new_content.replace(DEFAULT_PREFIX.upper(), NEW_PREFIX.upper())
-    # New file's name creation
-    new_file_name = os.path.basename(file_path).replace(DEFAULT_PREFIX, NEW_PREFIX)
+    if len(sys.argv) == 5:
+        file_path = os.path.join(sys.argv[4], os.path.basename(file_path))
+
+    new_file_name = os.path.basename(file_path).replace(DEFAULT_PREFIX, new_prefix)
 
     new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
+
     with open(new_file_path, "w") as dst_fp:
         print(new_file_path)
-
         dst_fp.write(new_content)
 
 
-if __name__ == "__main__":
-    input_file = sys.argv[1]
+def regenerate_content(file_content):
+    new_content = file_content.replace(DEFAULT_TYPE, new_type)
 
+    new_content = new_content.replace(
+        DEFAULT_PREFIX.lower(), new_prefix.lower()
+    ).replace(DEFAULT_PREFIX.upper(), new_prefix.upper())
+    return new_content
+
+
+if __name__ == "__main__":
     files_to_gen = [
-        os.path.join(_SRC_DIR, Path(input_file).stem + ".c"),
-        os.path.join(_HEADERS_DIR, Path(input_file).stem + ".h"),
+        os.path.join(_SRC_DIR, Path(file_path).stem + ".c"),
+        os.path.join(_HEADERS_DIR, Path(file_path).stem + ".h"),
     ]
 
     for file_ in files_to_gen:
